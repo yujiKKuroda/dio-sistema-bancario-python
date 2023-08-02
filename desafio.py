7a34fe1c3e46233a7e36becd82e43aa5e768a9d2
Rodrigo Yuji Koga Kuroda <83729536+RodrigoYujiKogaKuroda@users.noreply.github.com>
2023-08-02 14:07:10 -0300

    fix: adjust 'Sacar' action maximum limit

--- desafio.py
+++ desafio.py
@@ -21,6 +21,7 @@ while True:
         while True: 
             deposito = input("Quanto você quer depositar?\n=> ")
             deposito_float = float(deposito)
+
             if deposito_float > 0:
                 saldo += deposito_float
                 extrato += f"Depósito = +R${deposito_float:.2f}\n"
@@ -31,24 +32,26 @@ while True:
 
     elif opcao == "s":
         while True:
-            if numero_saques <= LIMITE_SAQUES:
+            if numero_saques < LIMITE_SAQUES:
                 saque = input("Quanto você quer sacar?\n=> ")
                 saque_float = float(saque)
 
-                if saque_float > limite:
-                    print("Valor muito alto, máximo de R$500.00 por transação! Por favor tente novamente!")
-                elif saque_float > 0:
-                    if (saque_float <= saldo):
-                        saldo -= saque_float
-                        extrato += f"Saque = +R${saque_float:.2f}\n"
-                        print("Saque bem-sucedido!")
-                        break
+                if saque_float > 0:
+                    if saque_float <= limite:
+                        if saque_float <= saldo:
+                            numero_saques += 1
+                            saldo -= saque_float
+                            extrato += f"Saque = -R${saque_float:.2f}\n"
+                            print("Saque bem-sucedido!")
+                            break
+                        else:
+                            print("Saldo insuficiente, tente novamente!")
                     else:
-                        print("Saldo insuficiente, tente novamente!")
+                        print("Valor muito alto, máximo de R$500.00 por transação! Por favor tente novamente!")
                 else:
                     print("Valor inválido, por favor tente novamente!")
             else:
-                print("Você alcançou o máximo de saques hoje, não tente novamente!")
+                print("Você alcançou o máximo de saques hoje, tente novamente outro dia!")
                 break
 
     elif opcao == "e":
